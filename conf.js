c4c17a62294a2026162b153c18063da5d05fb78c
Tom Hudson <073349@telent.local>
2017-05-18 10:00:21 +0100

    Added Firefox issue highlighting of issue with browser.navigate().back()

--- conf.js
+++ conf.js
@@ -1,11 +1,26 @@
-// An example configuration file.
+var seleniumDirectory = "./node_modules/protractor/node_modules/webdriver-manager/selenium/";
+
 exports.config = {
-  directConnect: true,
+  seleniumServerJar: `${seleniumDirectory}selenium-server-standalone-3.4.0.jar`,
+  localSeleniumStandaloneOpts: {
+    port: 4444,
+    jvmArgs: [
+      `-Dwebdriver.ie.driver=${seleniumDirectory}IEDriverServer3.4.0.exe`,
+      `-Dwebdriver.gecko.driver=${seleniumDirectory}geckodriver-v0.16.1.exe`,
+      `-Dwebdriver.chrome.driver=${seleniumDirectory}chromedriver_2.29.exe`]
+  },
 
   // Capabilities to be passed to the webdriver instance.
-  capabilities: {
+  multiCapabilities: [{
+    'browserName': 'internet explorer'
+  }, {
     'browserName': 'chrome'
-  },
+  }, {
+    'browserName': 'firefox'
+  }],
+
+  // Restrict no. of browser running at once so it is easier to see the problem!
+  maxSessions: 1,
 
   // Framework to use. Jasmine is recommended.
   framework: 'jasmine',
--- example_spec.js
+++ example_spec.js
@@ -1,11 +1,28 @@
-describe('protractor', function() {
-  it('<description of your problem>', function() {
-    browser.get('http://www.angularjs.org');
+var originalLocation = "https://stackoverflow.com/"
 
-    element(by.model('yourName')).sendKeys('Julie');
+describe('protractor', function () {
+  describe('when browser.navigate().back() is called', function () {
+    beforeAll(function () {
+      // Non angular example...
+      browser.ignoreSynchronization = true;
 
-    var greeting = element(by.binding('yourName'));
+      // Go to GitHub
+      browser.get(originalLocation);
 
-    expect(greeting.getText()).toEqual('Hello Julie!');
+      // Click the Log in link
+      browser.wait(ExpectedConditions.visibilityOf(element(by.linkText("Log In"))));
+      element(by.linkText("Log In")).click();
+
+      // Wait for Log In page to load
+      browser.wait(ExpectedConditions.visibilityOf(element(by.id("email"))));
+
+      // Trigger Browser Back button functionality
+      browser.navigate().back();
+    });
+
+    it('should take you back to the previous location', function () {
+      // Wait until the otiginal;
+      browser.wait(ExpectedConditions.urlIs(originalLocation), 10000, "browser.navigate().back()")
+    });
   });
 });
